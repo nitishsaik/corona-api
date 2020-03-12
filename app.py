db098f219ced1ba55b6744e34eed6420545479fb
kommaraju nitish sai <kommaraju.nitish.9@gmail.com>
2020-03-12 12:11:41 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask
+from flask import jsonify
+import carona
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return carona.Json_carona
+if __name__=="__main__":
+    app.run(debug=True,host='127.0.0.1',port=4444)
---
+++ carona.py
@@ -0,0 +1,32 @@
+from bs4 import BeautifulSoup as bs
+from urllib.request import urlopen as ur
+import requests
+import json
+
+URL='https://www.worldometers.info/coronavirus/'
+headers={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'}
+page=requests.get(URL,headers=headers)
+soup=bs(page.content,'html.parser')
+table_body=soup.find('table')
+rows = table_body.find_all('tr')
+l=[]
+d={
+    "carona":[]
+    }
+for row in rows:
+    cols=row.find_all('td')
+    z=['0' if v.text.strip() == "" else v.text.strip() for v in cols]
+
+    #print(z)
+    if len(z)!=0:
+        c,totc,newc,totd,newd,rec=z[:6]
+        d['carona'].append({
+            "country":c,
+            "totalcount":totc,
+            "newcount":newc,
+            "deaths":totd,
+            "newdeaths":newc,
+            "recoverd":rec
+        })
+Json_carona=json.dumps(d,indent=4)
+#print(Json_carona)
