01435b7a1dbf833e84655f2783cae2cc38ee7adc
kommaraju nitish sai <kommaraju.nitish.9@gmail.com>
2020-03-13 09:31:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask import jsonify
 import carona
 app = Flask(__name__)
-@app.route('/')
+@app.route('/app/api/v1.0/fetch', methods=['GET'])
 def index():
     return carona.Json_carona
 if __name__=="__main__":
